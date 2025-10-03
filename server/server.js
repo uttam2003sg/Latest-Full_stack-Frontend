247df77ea33cb1dc5975d6d2f7ca0d7c48cabd68
uttam2003sg <uttamgupta2003sg@gmail.com>
2025-10-04 00:11:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,9 +11,7 @@ const errorMiddleware = require("./middlewares/error-middleware");
 
 // let's tackle cors
 const corsOptions = {
-  origin: "http://localhost:5173",
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,
+  origin: '*',
 };
 
 app.use(cors(corsOptions));
